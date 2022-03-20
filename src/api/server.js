9f37e5ad0305aa56ad0c363efaeaa70154aa9c92
Anton Istomin <istomanton@gmail.com>
2022-03-20 14:33:04 +0300

    Handle search params in server, delete client.js

--- src/api/server.js
+++ src/api/server.js
@@ -1,12 +1,16 @@
 import { rest, setupWorker } from 'msw'
 import { factory, primaryKey } from '@mswjs/data'
 import { nanoid } from '@reduxjs/toolkit'
-import { Server as MockSocketServer } from 'mock-socket'
 
 const NUM_ITEMS = 10
 
-// Add an extra delay to all endpoints, so loading spinners show up.
-const ARTIFICIAL_DELAY_MS = 2000
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * max);
+}
+
+const getDelay = (minSeconds, maxSeconds) => {
+  return (getRandomInt(maxSeconds - minSeconds + 1) + minSeconds) * 1000;
+}
 
 export const db = factory({
   item: {
@@ -20,24 +24,15 @@ for (let i = 0; i < NUM_ITEMS; i++) {
   db.item.create(newItem)
 }
 
-const serializeItem = (item) => item.name
-
 export const handlers = [
   rest.get('/fakeApi/items', function (req, res, ctx) {
-    const items = db.item.getAll().map(serializeItem)
-    return res(ctx.delay(ARTIFICIAL_DELAY_MS), ctx.json(items))
+    const name = req.url.searchParams.get('name')
+    const items = db.item.findMany({
+      where: { name: { contains: name } }
+    })
+    return res(ctx.delay(getDelay(2, 4)), ctx.json(items))
   }),
 ]
 
 export const worker = setupWorker(...handlers)
 // worker.printHandlers() // Optional: nice for debugging to see all available route handlers that will be intercepted
-
-/* Mock Websocket Setup */
-
-const socketServer = new MockSocketServer('ws://localhost')
-
-let currentSocket
-
-const sendMessage = (socket, obj) => {
-  socket.send(JSON.stringify(obj))
-}
--- src/features/item/itemAPI.js
+++ src/features/item/itemAPI.js
@@ -1,26 +1,3 @@
-import { client } from "../../api/client";
-
-const items = [
-  { name: 'item1' },
-  { name: 'item2' },
-  { name: 'item3' },
-  { name: 'item4' },
-  { name: 'item5' },
-  { name: 'item6' },
-]
-
-const getRandomInt = (max) => {
-  return Math.floor(Math.random() * max);
-}
-
-const getDelay = (minSeconds, maxSeconds) => {
-  return (getRandomInt(maxSeconds - minSeconds + 1) + minSeconds) * 1000;
-}
-
 export const fetchItems = async (name) => {
-  const response = await client.get('/fakeApi/items')
-  console.log(response.data)
-  const delay = getDelay(2, 4);
-  const filteredItems = items.filter(i => i.name.includes(name));
-  return new Promise((resolve, reject) => setTimeout(() => resolve(filteredItems), delay));
+  return fetch(`/fakeApi/items?name=${name}`).then(response => response.json())
 }
